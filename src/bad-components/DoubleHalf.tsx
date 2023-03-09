0cc15c23bd19592d3056928575cf25399b397188
Anirudh Naredla <anirudhn@udel.edu>
2023-03-09 14:10:29 -0500

    coloredBox and DoubleHalf run well, task 9

--- src/bad-components/DoubleHalf.tsx
+++ src/bad-components/DoubleHalf.tsx
@@ -1,24 +1,35 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
-import { dhValue, setDhValue } from "./DoubleHalfState";
+//import { dhValue, setDhValue } from "./DoubleHalfState";
 
-function Doubler(): JSX.Element {
-    return <Button onClick={() => setDhValue(2 * dhValue)}>Double</Button>;
-}
+// function Doubler(): JSX.Element {
+//     return <Button onClick={() => setDhValue(2 * dhValue)}>Double</Button>;
+// }
 
-function Halver(): JSX.Element {
-    return <Button onClick={() => setDhValue(0.5 * dhValue)}>Halve</Button>;
-}
+// function Halver(): JSX.Element {
+//     return <Button onClick={() => setDhValue(0.5 * dhValue)}>Halve</Button>;
+// }
 
 export function DoubleHalf(): JSX.Element {
+    const [dhValue, setDhValue] = useState<number>(10);
+    function doubleVal(): void {
+        setDhValue(dhValue * 2);
+    }
+    function halfVal(): void {
+        setDhValue(dhValue / 2);
+    }
     return (
         <div>
             <h3>Double Half</h3>
             <div>
                 The current value is: <span>{dhValue}</span>
             </div>
-            <Doubler></Doubler>
-            <Halver></Halver>
+            {/* <Doubler></Doubler>
+            <Halver></Halver> */}
+            <Button onClick={doubleVal}>Double</Button>
+            <Button onClick={halfVal}>Halve</Button>
         </div>
     );
 }
+
+//AM I ALLOWED TO EVEN DO IT THIS WAY??? WILL THE TESTS BUG OUT???
--- src/bad-components/ShoveBox.tsx
+++ src/bad-components/ShoveBox.tsx
@@ -33,18 +33,18 @@ function MoveableBox(): JSX.Element {
 
 export function ShoveBox(): JSX.Element {
     const box = MoveableBox();
-
+    const [position, setPosition] = useState<number>(10);
     return (
         <div>
             <h3>Shove Box</h3>
-            {/* <span>The box is at: {box.position}</span>
+            <span>The box is at: {box.position}</span>
             <div>
                 <ShoveBoxButton
                     position={box.position}
                     setPosition={box.setPosition}
                 ></ShoveBoxButton>
                 {box}
-            </div> */}
+            </div>
         </div>
     );
 }
